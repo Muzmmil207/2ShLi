60fc0f809c7e636cc4aa5a8f61b7b0b4457e4340
modather <modather0ali@gmail.com>
2022-06-29 17:42:57 +0200

    prepare for hosting

--- shli/urls.py
+++ shli/urls.py
@@ -19,6 +19,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from account import views
 
+from django.views.static import serve
+from django.conf.urls import url
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('<str:token>', views.TheLink, name="TheLink"),
@@ -27,5 +31,7 @@ urlpatterns = [
     path('registrations/', include('django.contrib.auth.urls')),
     path('registrations/', include('registrations.urls')),
 
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
